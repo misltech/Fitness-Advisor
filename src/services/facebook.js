12e53c0a3f0669b21587d16ab2cfc811b3178069
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2018-12-03 23:24:09 -0500

    errors after erros

---
+++ src/services/facebook.js
@@ -0,0 +1,32 @@
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '191023585134452',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
+
+   function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  function getScopes(){
+      return "email, user_birthdaym user_location, public_profile";
+  }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,35 @@
   <div class="home">
     <div class="jumbotron">
       <h1 class="display-4">Hello, world!</h1>
-      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <p
+        class="lead"
+      >This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
       <hr class="my-4">
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      <p> add facebook button right here and rename the rest of these stuff</p>
+      <p>add facebook button right here and rename the rest of these stuff</p>
+      <div
+        class="fb-login-button"
+        data-max-rows="1"
+        data-size="large"
+        data-button-type="continue_with"
+        data-show-faces="false"
+        data-auto-logout-link="false"
+        data-use-continue-as="false"
+        data-scope="{{getScope()}}"
+      >></div>
     </div>
   </div>
 </template>
+
+<script>
+import * as fb from "@./services/facebook";
+
+export default {
+  methods: {
+    getScope() {
+      return fb.getScopes();
+    }
+  }
+};
+</script>
